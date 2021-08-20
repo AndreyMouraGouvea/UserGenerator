04b03723a0af8cf17736f24da6fdc803bf40b5f9
AndreyMouraGouvea <andreygouveia.05@gmail.com>
2021-08-19 23:52:53 -0300

    Clients Area Created/Updated

--- CadastroArray/frmPrincipal.cs
+++ CadastroArray/frmPrincipal.cs
@@ -20,10 +20,23 @@ namespace CadastroArray
             public string login;
             public string senha;
         }
+        public struct Cliente
+        {
+            public int codigo;
+            public string nome;
+            public int telefone;
+            public string email;
+            public int cpf;
+        }
         //array vetor declarado
 
         static public Usuario[] usuarios = new Usuario[10];
         static public int cadusu = 0;
+
+        //cadastrando cliente
+
+        static public Cliente[] clientes = new Cliente[10];
+        static public int cadcli = 0;
 
         public frmPrincipal()
         {
@@ -39,6 +52,11 @@ namespace CadastroArray
         {
             frmUsuario fu = new frmUsuario();
             fu.Show();
+        }
+        private void clienteToolStripMenuItem(object sender, EventArgs e)
+        {
+            frmCliente fc = new frmCliente();
+            fc.Show();
         }
     }
 }
